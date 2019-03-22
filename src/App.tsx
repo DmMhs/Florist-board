24c05c75e3a292e0151059a704a3df3cd82d5ace
Dm Mh <dmmhs888@gmail.com>
2019-03-22 23:52:48 +0200

    Add mobile navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,41 @@ import contacts from './components/Contacts/Contacts';
 import Home from './components/Home/Home';
 
 class App extends Component {
+  state = {
+    showNavigation: false
+  }
+  
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth < 576) {
+        this.setState({
+          showNavigation: false
+        });
+      } else {
+        this.setState({
+          showNavigation: true
+        });
+      }
+    });
+  }
+
+  toggleClickedHandler = () => {
+    this.setState({
+      showNavigation: !this.state.showNavigation
+    });
+    if (this.state.showNavigation) {
+      document.getElementById('toggle')!.classList.add('active');
+    } else {
+      document.getElementById('toggle')!.classList.remove('active');
+    }
+  }
+  
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Header />
+        <i className="fas fa-ellipsis-h" id="toggle" onClick={this.toggleClickedHandler}></i>
+          { this.state.showNavigation ? <Header /> : null }
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/shop" component={Shop} />
