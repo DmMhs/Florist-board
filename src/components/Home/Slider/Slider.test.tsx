e56eb06993ad0f216b63132777aa936c9d15cf1d
Dm Mh <dmmhs888@gmail.com>
2019-03-24 12:00:31 +0200

    Add unit tests

---
+++ src/components/Home/Slider/Slider.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import  {shallow} from 'enzyme';
+import ReactDOM from 'react-dom';
+
+import Slider from './Slider';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Slider />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has a valid state value', () => {
+  const wrapper = shallow(<Slider />);
+  const instance = wrapper.instance();
+  expect(instance.state.images.length).toBeGreaterThanOrEqual(1);
+  expect(instance.state.translateValue).toBeGreaterThanOrEqual(0);
+  expect(instance.state.timerInterval).toBeGreaterThan(0);
+  expect(instance.state.currentIndex).not.toBeNull();
+});
