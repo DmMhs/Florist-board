d7814ceecec8ee56d10c2531d1ad057ea57eeb8a
Dmytro Mahas <dmytro.mahas@eleks.com>
2019-05-06 17:59:57 +0300

    Removed function

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -11,20 +11,9 @@ admin.initializeApp({
   databaseURL: 'https://florist-cb933.firebaseio.com'
 });
 
-export const giveAdminRole = functions.https.onRequest(
-  async (request, response) => {
-    response.set('Access-Control-Allow-Origin', '*');
-    const user = await admin.auth().getUserByEmail(request.params.email);
-    if (user.customClaims && (user.customClaims as any).admin === true) {
-      response.send('NOICE!');
-      return;
-    }
-    response.send("You're ADMIN");
-    // return admin.auth().setCustomUserClaims(user.uid, {
-    //   admin: true
-    // });
-  }
-);
+export const giveAdminRole = functions.https.onRequest((request, response) => {
+  response.send('Is admin: ' + request.params.admin);
+});
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
