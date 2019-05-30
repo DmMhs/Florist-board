c9757cbe5ccd82bac25db497e365d2028f297d63
Dmytro Mahas <dmytro.mahas@eleks.com>
2019-05-30 18:36:54 +0300

    Improved shop UI and removed rudimentary files

--- src/services/shop/share.ts
+++ src/services/shop/share.ts
@@ -4,11 +4,13 @@ export const shareOverrideOGMeta = (
   overrideDescription: string,
   overrideImage: string
 ) => {
+  console.log(overrideLink, overrideTitle, overrideDescription, overrideImage);
   const params: fb.ShareOpenGraphDialogParams = {
     method: 'share_open_graph',
     action_type: 'og.likes',
     action_properties: JSON.stringify({
       object: {
+        'og:url': overrideLink,
         'og:title': overrideTitle.toUpperCase(),
         'og:description': overrideDescription,
         'og:image': overrideImage
